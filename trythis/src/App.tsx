841f2cf3fb6ed1c80d52758f8458e5b5fc2e4f29
rhtjddls123 <jjjk0605@naver.com>
2023-12-12 23:52:37 +0900

    useEffect를 사용해 strictMode에서 1sec에 1count증가 good case, bad case make

--- trythis/src/App.tsx
+++ trythis/src/App.tsx
@@ -3,13 +3,34 @@ import Hello from './components/Hello';
 import My from './components/My';
 import './App.css';
 import { useCounter } from './hooks/counter-context';
+import { useState, useEffect } from 'react';
 
 function App() {
   console.log('@@@App');
   const { count } = useCounter();
 
+  const [badSec, setBadSec] = useState(0);
+  const [goodSec, setGoodSec] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setBadSec((pre) => pre + 1);
+    }, 1000);
+  }, []);
+  useEffect(() => {
+    const goodSet = setInterval(() => {
+      setGoodSec((pre) => pre + 1);
+    }, 1000);
+    return () => {
+      clearInterval(goodSet);
+    };
+  }, []);
+
   return (
     <>
+      <span style={{ float: 'left', color: 'red' }}>{badSec} sec</span>
+      <span style={{ float: 'right', color: 'green' }}>{goodSec} sec</span>
+
       <h2>count: {count}</h2>
       <My />
       <Hello name='홍길동' age={30}>
