4e8cea7295e5bb281f8e6ea9f5e3e6a0d08c65b4
rhtjddls123 <jjjk0605@naver.com>
2023-12-26 21:17:10 +0900

    refactoring

--- trythis/src/components/ItemLayout.tsx
+++ trythis/src/components/ItemLayout.tsx
@@ -1,6 +1,6 @@
 import { useSession } from '../hooks/session-context';
 import { Outlet, useSearchParams } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const ItemLayout = () => {
   // const [name, setName] = useState<string>('');
@@ -10,8 +10,28 @@ export const ItemLayout = () => {
     removeCartItem,
     addCart,
   } = useSession();
-  const [searchParams, setSearchParams] = useSearchParams({ searchStr: '' });
+  const [items, setItems] = useState<Cart[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams({
+    searchStr: '',
+    itemId: '',
+  });
   const [currItem, setCurrItem] = useState<Cart | null>();
+
+  const searchStr = searchParams.get('searchStr') || '';
+  const itemId = searchParams.get('itemId') || '';
+  items.sort((a, b) => {
+    return b.id - a.id;
+  });
+
+  useEffect(() => {
+    setItems(cart.filter((item) => item.name.includes(searchStr)));
+  }, [cart, searchStr]);
+
+  useEffect(() => {
+    if (itemId)
+      setCurrItem(cart.find((item) => item.id === Number(itemId)) || null);
+    else setCurrItem(items[0]);
+  }, [items, cart, itemId]);
 
   return (
     <>
@@ -24,37 +44,38 @@ export const ItemLayout = () => {
       <br />
       <div
         style={{
-          display: 'inline-flex',
+          margin: '1rem',
+          display: 'grid',
+          gridTemplateColumns: '2fr 2fr',
+          gap: '1em',
+          width: '80vw',
         }}
       >
         <div>
-          <ul style={{ height: '200px', overflow: 'auto' }}>
-            {cart
-              .filter((item) =>
-                item.name.includes(searchParams.get('searchStr') || '')
-              )
-              .map((item: Cart) => (
-                <li key={item.id}>
-                  <small>{item.id}</small>
-                  <button
-                    onClick={() => {
-                      setCurrItem(item);
-                    }}
-                  >
-                    {item.name}
-                  </button>
+          <ul style={{ height: '200px', overflow: 'auto', listStyle: 'none' }}>
+            {items.map((item: Cart) => (
+              <li key={item.id}>
+                <small>{item.id}</small>
+                <button
+                  onClick={() => {
+                    setCurrItem(item);
+                    setSearchParams({ searchStr, itemId: String(item.id) });
+                  }}
+                >
+                  {item.name}
+                </button>
 
-                  <small>({item.price.toLocaleString()}원)</small>
-                  <button
-                    onClick={() => {
-                      removeCartItem(item.id);
-                      setCurrItem(null);
-                    }}
-                  >
-                    DEL
-                  </button>
-                </li>
-              ))}
+                <small>({item.price.toLocaleString()}원)</small>
+                <button
+                  onClick={() => {
+                    removeCartItem(item.id);
+                    setCurrItem(null);
+                  }}
+                >
+                  DEL
+                </button>
+              </li>
+            ))}
           </ul>
           <button
             onClick={() => {
@@ -64,7 +85,9 @@ export const ItemLayout = () => {
             ADD
           </button>
         </div>
-        <Outlet context={{ currItem, addCart, removeCartItem }} />
+        <Outlet
+          context={{ item: currItem, addCart, removeCartItem, setSearchParams }}
+        />
       </div>
     </>
   );
--- trythis/src/components/item.tsx
+++ trythis/src/components/item.tsx
@@ -1,61 +1,40 @@
-import { useOutletContext } from 'react-router-dom';
-import { useEffect, useRef, useState } from 'react';
+import { useOutletContext, useSearchParams } from 'react-router-dom';
+import { useEffect, useReducer, useRef } from 'react';
 
 type ChildProps = {
-  currItem: Cart | null;
+  item: Cart;
   addCart: (id: number, name: string, price: number) => void;
   removeCartItem: (itemId: number) => void;
 };
 
 export const Item = () => {
-  const { currItem, addCart, removeCartItem } = useOutletContext<ChildProps>();
-  const [item, setItem] = useState<Cart | null>();
+  const { item, addCart, removeCartItem } = useOutletContext<ChildProps>();
+  const [isEditing, toggleEditing] = useReducer((pre) => !pre, false);
   const nameRef = useRef<HTMLInputElement>(null);
   const priceRef = useRef<HTMLInputElement>(null);
-  const curr = {
-    id: currItem?.id,
-    name: currItem?.name,
-    price: currItem?.price,
-  };
-  console.log(sessionStorage);
+  const [, setSearchParams] = useSearchParams({ searchStr: '' });
 
   useEffect(() => {
-    setItem(currItem);
-    if (nameRef.current && priceRef.current && currItem) {
-      nameRef.current.value = currItem.name;
-      priceRef.current.value = '' + currItem.price;
-    } else if (nameRef.current && priceRef.current && !currItem) {
-      nameRef.current.value = '';
-      priceRef.current.value = '';
+    if (nameRef.current && priceRef.current && item) {
+      nameRef.current.value = item.name;
+      priceRef.current.value = '' + item.price;
     }
-  }, [currItem]);
+  }, [item, isEditing]);
 
   const edit = () => {
-    if (
-      !nameRef.current?.value ||
-      !priceRef.current?.value ||
-      (nameRef.current?.value === curr.name &&
-        priceRef.current?.value === '' + curr.price)
-    ) {
-      alert('제대로 입력하세요');
-    } else if (nameRef.current?.value && priceRef.current?.value) {
-      addCart(
-        Number(item?.id),
-        nameRef.current.value,
-        Number(priceRef.current.value)
-      );
-      if (!item?.id) {
-        setItem(null);
-        nameRef.current.value = '';
-        priceRef.current.value = '';
+    if (isEditing) {
+      if (nameRef.current && priceRef.current) {
+        addCart(item?.id || 0, nameRef.current.value, +priceRef.current.value);
+        setSearchParams({ searchStr: nameRef.current.value });
       }
     }
+
+    toggleEditing();
   };
 
   const remove = () => {
     if (item) {
       removeCartItem(item.id);
-      setItem(null);
     }
     if (nameRef.current && priceRef.current) {
       nameRef.current.value = '';
@@ -65,22 +44,31 @@ export const Item = () => {
 
   return (
     <>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <div style={{ margin: 'auto' }}>
-          id: {item?.id}번 name: {item?.name} price:{' '}
-          {item?.price.toLocaleString()}원
-          {item?.id && <button onClick={() => remove()}>X</button>}
-        </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          border: '2px solid green',
+          justifyContent: 'center',
+        }}
+      >
         <div>
-          상품명: <input type='text' ref={nameRef} /> 가격:{' '}
-          <input type='number' ref={priceRef} />{' '}
-          <button
-            onClick={() => {
-              edit();
-            }}
-          >
-            {item?.id ? 'Edit' : 'Add'}
-          </button>
+          {!isEditing ? (
+            <div>
+              id: {item?.id}번 name: {item?.name} price:{' '}
+              {item?.price.toLocaleString()}원
+              {item?.id && <button onClick={() => remove()}>X</button>}
+            </div>
+          ) : (
+            <div>
+              <input type='text' ref={nameRef} />
+              <br />
+              <input type='number' ref={priceRef} />
+              <br />
+            </div>
+          )}
+          {isEditing && <button onClick={toggleEditing}>Cancel</button>}
+          <button onClick={edit}>{isEditing ? 'Save' : 'Edit'}</button>
         </div>
       </div>
     </>
