a18a304f9e96c64d54793f5f5499da3254c83ffe
rhtjddls123 <jjjk0605@naver.com>
2023-12-27 16:39:50 +0900

    CompBox useState 1번만 사용하여 mount unmount

--- practice/src/components/CompBox.tsx
+++ practice/src/components/CompBox.tsx
@@ -5,9 +5,7 @@ import CompB from './CompB';
 import CompC from './CompC';
 
 const CompBox = () => {
-  const [toggleA, setToggleA] = useState(false);
-  const [toggleB, setToggleB] = useState(false);
-  const [toggleC, setToggleC] = useState(false);
+  const [toggles, setToggles] = useState({ A: false, B: false, C: false });
   return (
     <Box
       borderWidth='2px'
@@ -16,13 +14,31 @@ const CompBox = () => {
       padding='4px'
       margin='2px'
     >
-      <button onClick={() => setToggleA((pre) => !pre)}>toggleA</button>
-      <button onClick={() => setToggleB((pre) => !pre)}>toggleB</button>
-      <button onClick={() => setToggleC((pre) => !pre)}>toggleC</button>
+      <button
+        onClick={() =>
+          setToggles({ A: !toggles.A, B: toggles.B, C: toggles.C })
+        }
+      >
+        toggleA
+      </button>
+      <button
+        onClick={() =>
+          setToggles({ A: toggles.A, B: !toggles.B, C: toggles.C })
+        }
+      >
+        toggleB
+      </button>
+      <button
+        onClick={() =>
+          setToggles({ A: toggles.A, B: toggles.B, C: !toggles.C })
+        }
+      >
+        toggleC
+      </button>
       <br />
-      {toggleA && <CompA />}
-      {toggleB && <CompB />}
-      {toggleC && <CompC />}
+      {toggles.A && <CompA />}
+      {toggles.B && <CompB />}
+      {toggles.C && <CompC />}
     </Box>
   );
 };
