22d775f491bf4cde20dcb9827e93a17ed2cd17af
rhtjddls123 <jjjk0605@naver.com>
2024-03-27 17:18:07 +0900

    Feat: Player name변경 기능 추가
    useState를 사용하여 playerName과 submit상태 변경
    useRef를 사용하여 input입력 시 렌더링 최적화

--- tic-tac-toe/src/components/Player.tsx
+++ tic-tac-toe/src/components/Player.tsx
@@ -1,16 +1,31 @@
+import { useRef, useState } from "react";
+
 type Props = {
   name: string;
   symbol: string;
 };
 
 const Player = ({ name, symbol }: Props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState<string | undefined>(name);
+  const nameRef = useRef<HTMLInputElement>(null);
+
+  const handleEditClick = () => {
+    setIsEditing((pre) => !pre);
+    if (isEditing) setPlayerName(nameRef.current?.value);
+  };
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? (
+          <input type="text" ref={nameRef} defaultValue={playerName} required />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 };
