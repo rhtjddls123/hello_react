5dc658e26eee60affdc38fd4e347e5baaacfa670
rhtjddls123 <jjjk0605@naver.com>
2024-03-27 17:41:39 +0900

    Feat: GameBoard click event추가
    button click시 X로 바뀌는 기능 추가

--- tic-tac-toe/src/components/GameBoard.tsx
+++ tic-tac-toe/src/components/GameBoard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,28 @@ const initialGameBoard = [
 ];
 
 const GameBoard = () => {
+  const [gameBoard, setGameBoard] =
+    useState<(string | null)[][]>(initialGameBoard);
+
+  const handleSelectSquare = (rowIndex: number, colIndex: number) => {
+    setGameBoard((prevGameBoard) => {
+      const updateGameBoard: (string | null)[][] = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updateGameBoard[rowIndex][colIndex] = "X";
+      return updateGameBoard;
+    });
+  };
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
