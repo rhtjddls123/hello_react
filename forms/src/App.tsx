3fc3d0eb7e32404a65a3f0f408ad3ca1c27478f3
rhtjddls123 <jjjk0605@naver.com>
2024-10-02 21:52:09 +0900

    forms init

---
+++ forms/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+}
+
+export default App;
