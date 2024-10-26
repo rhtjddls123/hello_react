d38f34d7d828a8d2c993e8a19ebc735258557286
rhtjddls123 <jjjk0605@naver.com>
2024-10-27 00:27:36 +0900

    react-router basic 실습

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDeatilPage from "./pages/ProductDeatil";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "products",
+        element: <ProductsPage />
+      },
+      { path: "products/:productId", element: <ProductDeatilPage /> }
+    ]
+  }
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ react-router/src/types/paramsType.d.ts
@@ -0,0 +1,3 @@
+export type ParamsType = {
+  productId: string;
+};
