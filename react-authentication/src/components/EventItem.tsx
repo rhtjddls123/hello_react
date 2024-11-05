91adefd9f8b2a252d611b5fcb6e6cb32328ec972
rhtjddls123 <jjjk0605@naver.com>
2024-11-05 16:59:26 +0900

    project init

---
+++ react-authentication/src/components/EventItem.tsx
@@ -0,0 +1,33 @@
+import { Link, useSubmit } from "react-router-dom";
+import classes from "./EventItem.module.css";
+
+interface EventItemProps {
+  event: EventType;
+}
+
+function EventItem({ event }: EventItemProps) {
+  const submit = useSubmit();
+
+  function startDeleteHandler() {
+    const proceed = window.confirm("Are you sure?");
+
+    if (proceed) {
+      submit(null, { method: "DELETE" });
+    }
+  }
+
+  return (
+    <article className={classes.event}>
+      <img src={event.image} alt={event.title} />
+      <h1>{event.title}</h1>
+      <time>{event.date}</time>
+      <p>{event.description}</p>
+      <menu className={classes.actions}>
+        <Link to="edit">Edit</Link>
+        <button onClick={startDeleteHandler}>Delete</button>
+      </menu>
+    </article>
+  );
+}
+
+export default EventItem;
